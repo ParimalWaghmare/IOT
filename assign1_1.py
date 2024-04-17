77bb136a25d819b1114dafbdf9498c2e27a1b1e9
ParimalWaghmare <pariwaghmare32@gmail.com>
2024-04-17 07:49:38 +0530

    assignment

---
+++ assign1_1.py
@@ -0,0 +1,9 @@
+
+length = float (input("Enter the length\n"))
+breadth = float(input("Enter breadth \n"))
+
+area = length * breadth
+print(f"Area of Rectangle = {area}")
+
+perimeter = float(2*(length+breadth))
+print(f"Perimeter is = {perimeter}")
